99dec579d1aa08c4dbeb83703b6d794bf00e3e99
destecx <destecx@gmail.com>
2018-01-25 23:34:00 +0800

    feat: add ipfs network bootstrap services

---
+++ app/service/ipfs/network/bootstrap.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const egg_1 = require("egg");
+const IPFS = require("ipfs-api");
+class IpfsNetworkBootstrapService extends egg_1.Service {
+    constructor(ctx) {
+        super(ctx);
+        if (!this.ctx.service.ipfs) {
+            this.ipfs = IPFS({
+                host: this.config.ipfs.host,
+                port: this.config.ipfs.port,
+                protocol: this.config.ipfs.protocol,
+            });
+        }
+    }
+}
+exports.default = IpfsNetworkBootstrapService;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYm9vdHN0cmFwLmpzIiwic291cmNlUm9vdCI6Ii9zcmMvIiwic291cmNlcyI6WyJzZXJ2aWNlL2lwZnMvbmV0d29yay9ib290c3RyYXAudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSw2QkFBdUM7QUFDdkMsaUNBQWlDO0FBS2pDLGlDQUFpRCxTQUFRLGFBQU87SUFHOUQsWUFBWSxHQUFZO1FBQ3RCLEtBQUssQ0FBQyxHQUFHLENBQUMsQ0FBQztRQUNYLEVBQUUsQ0FBQyxDQUFDLENBQUMsSUFBSSxDQUFDLEdBQUcsQ0FBQyxPQUFPLENBQUMsSUFBSSxDQUFDLENBQUMsQ0FBQztZQUMzQixJQUFJLENBQUMsSUFBSSxHQUFHLElBQUksQ0FBQztnQkFDZixJQUFJLEVBQUUsSUFBSSxDQUFDLE1BQU0sQ0FBQyxJQUFJLENBQUMsSUFBSTtnQkFDM0IsSUFBSSxFQUFFLElBQUksQ0FBQyxNQUFNLENBQUMsSUFBSSxDQUFDLElBQUk7Z0JBQzNCLFFBQVEsRUFBRSxJQUFJLENBQUMsTUFBTSxDQUFDLElBQUksQ0FBQyxRQUFRO2FBQ3BDLENBQUMsQ0FBQztRQUNMLENBQUM7SUFDSCxDQUFDO0NBQ0Y7QUFiRCw4Q0FhQyJ9
