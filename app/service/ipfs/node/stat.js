c0ae6aada080f7cf85641b618062e094cb0ba9a9
destecx <destecx@gmail.com>
2018-01-27 23:11:14 +0800

    feat: add node stat services

---
+++ app/service/ipfs/node/stat.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const egg_1 = require("egg");
+const IPFS = require("ipfs-api");
+class IpfsNodeStatService extends egg_1.Service {
+    constructor(ctx) {
+        super(ctx);
+        if (!this.ctx.service.ipfs) {
+            this.ipfs = IPFS({
+                host: this.config.ipfs.host,
+                port: this.config.ipfs.port,
+                protocol: this.config.ipfs.protocol,
+            });
+        }
+    }
+}
+exports.default = IpfsNodeStatService;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdC5qcyIsInNvdXJjZVJvb3QiOiIvc3JjLyIsInNvdXJjZXMiOlsic2VydmljZS9pcGZzL25vZGUvc3RhdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDZCQUF1QztBQUN2QyxpQ0FBaUM7QUFLakMseUJBQXlDLFNBQVEsYUFBTztJQUd0RCxZQUFZLEdBQVk7UUFDdEIsS0FBSyxDQUFDLEdBQUcsQ0FBQyxDQUFDO1FBQ1gsRUFBRSxDQUFDLENBQUMsQ0FBQyxJQUFJLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsQ0FBQyxDQUFDO1lBQzNCLElBQUksQ0FBQyxJQUFJLEdBQUcsSUFBSSxDQUFDO2dCQUNmLElBQUksRUFBRSxJQUFJLENBQUMsTUFBTSxDQUFDLElBQUksQ0FBQyxJQUFJO2dCQUMzQixJQUFJLEVBQUUsSUFBSSxDQUFDLE1BQU0sQ0FBQyxJQUFJLENBQUMsSUFBSTtnQkFDM0IsUUFBUSxFQUFFLElBQUksQ0FBQyxNQUFNLENBQUMsSUFBSSxDQUFDLFFBQVE7YUFDcEMsQ0FBQyxDQUFDO1FBQ0wsQ0FBQztJQUNILENBQUM7Q0FDRjtBQWJELHNDQWFDIn0=
---
+++ src/service/ipfs/node/stat.ts
@@ -0,0 +1,20 @@
+import { Context, Service } from 'egg';
+import * as IPFS from 'ipfs-api';
+import { promisify } from 'util';
+
+export interface IIpfsNodeStatService {}
+
+export default class IpfsNodeStatService extends Service
+  implements IIpfsNodeStatService {
+  private ipfs: any;
+  constructor(ctx: Context) {
+    super(ctx);
+    if (!this.ctx.service.ipfs) {
+      this.ipfs = IPFS({
+        host: this.config.ipfs.host,
+        port: this.config.ipfs.port,
+        protocol: this.config.ipfs.protocol,
+      });
+    }
+  }
+}
