56c524d75f5d672e01dbb61d42682b70d541e848
destecx <destecx@gmail.com>
2018-02-02 23:15:03 +0800

    fix: fix index.d.ts for services

--- src/service/index.d.ts
+++ src/service/index.d.ts
@@ -1,18 +1,22 @@
 import { FileService } from './storage/files';
 import { QRService } from './misc/qr';
-import { IIpfsNodeMiscServiceIIpfsService } from './ipfs/node/misc';
-import { IIpfsNodeConfigService } from './ipfs/node/config';
+import { IIpfsNetworkBootstrapService } from './ipfs/network/bootstrap';
 import { IIpfsNetworkSwarmService } from './ipfs/network/swarm';
+import { IIpfsNodeConfigService } from './ipfs/node/config';
+import { IIpfsNodeMiscService } from './ipfs/node/misc';
+import { IIpfsNodeStatService } from './ipfs/node/stat';
 
 declare module 'egg' {
   export interface IService {
     ipfs: {
       network: {
+        bootstrap: IIpfsNetworkBootstrapService;
         swarm: IIpfsNetworkSwarmService;
       };
       node: {
         misc: IIpfsNodeMiscService;
         config: IIpfsNodeConfigService;
+        stat: IIpfsNodeStatService;
       };
     };
     misc: {
