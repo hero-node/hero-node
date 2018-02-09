d108bf76aff6edad2899137dc1725cabf2d77163
destecx <destecx@gmail.com>
2018-02-09 23:15:46 +0800

    feat:
    add files block get service function

---
+++ src/service/ipfs/files/block.ts
@@ -0,0 +1,34 @@
+import { Context, Service } from 'egg';
+import * as IPFS from 'ipfs-api';
+import { promisify } from 'util';
+
+export interface IIpfsFilesBlockService {
+  get: (cid) => Promise<any>;
+}
+
+export default class IpfsFilesBlockService extends Service
+  implements IIpfsFilesBlockService {
+  private ipfs: any;
+  constructor(ctx: Context) {
+    super(ctx);
+    if (!this.ctx.service.ipfs) {
+      this.ipfs = IPFS({
+        host: this.config.ipfs.host,
+        port: this.config.ipfs.port,
+        protocol: this.config.ipfs.protocol,
+      });
+    }
+  }
+
+  public async get(cid): Promise<any> {
+    try {
+      const getSync = promisify(this.ipfs.blcok.get);
+      const res = await getSync(cid);
+      this.ctx.logger.debug(res);
+      return res;
+    } catch (err) {
+      this.ctx.logger.warn(err);
+      return;
+    }
+  }
+}
