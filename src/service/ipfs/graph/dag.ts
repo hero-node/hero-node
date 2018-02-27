ef57942ae76ea9da447d095826e7fe3021f4f8a0
destecx <destecx@gmail.com>
2018-02-27 23:15:19 +0800

    feat: add graph dag service

---
+++ src/service/ipfs/graph/dag.ts
@@ -0,0 +1,20 @@
+import { Context, Service } from 'egg';
+import * as IPFS from 'ipfs-api';
+import { promisify } from 'util';
+
+export interface IIpfsGraphDagService {}
+
+export default class IpfsGraphDagService extends Service
+  implements IIpfsGraphDagService {
+  private ipfs: any;
+  constructor(ctx: Context) {
+    super(ctx);
+    if (!this.ctx.service.ipfs) {
+      this.ipfs = IPFS({
+        host: this.config.ipfs.host,
+        port: this.config.ipfs.port,
+        protocol: this.config.ipfs.protocol,
+      });
+    }
+  }
+}
