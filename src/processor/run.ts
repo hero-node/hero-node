8c8b19e31afc971bd5cd1eeded50063610f44279
destec <destecx@gmail.com>
2018-03-20 21:47:36 +0800

    fix for port parameter

--- src/processor/run.ts
+++ src/processor/run.ts
@@ -2,9 +2,10 @@ import * as _ from 'lodash';
 import { default as server } from '../server';
 import { LoggerFactory } from '../utils/logger';
 
-export default port => {
+export default options => {
   const logger = LoggerFactory.getLabeledInstance('processor', 'run');
-  if (!_.isNumber(+port) || port <= 0) port = 80;
+  let port = options.port;
+  if (_.isNaN(+port) || port <= 0) port = 80;
   else port = +port;
   logger.info(`server will start on port ${port}`);
   server.listen(port);
